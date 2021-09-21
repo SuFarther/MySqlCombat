58e58db1b0570ef976f7a7b419ee4aa7fe448aeb
xiaosu <2257346739@qq.com>
2021-09-21 08:57:10 +0800

    JDBC案例需要用到数据,以及JDBC封装数据库的模糊查询

--- jdbc/需要用到的文档/sql/test.sql
+++ jdbc/需要用到的文档/sql/test.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 20/09/2021 22:49:16
+ Date: 21/09/2021 08:55:37
 */
 
 SET NAMES utf8mb4;
@@ -45,7 +45,7 @@ CREATE TABLE `student` (
   `age` int DEFAULT NULL COMMENT '年龄',
   `email` varchar(255) DEFAULT NULL COMMENT '邮箱',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of student
@@ -59,6 +59,8 @@ INSERT INTO `student` VALUES (5, '猫咪', 31, '112@qq.com');
 INSERT INTO `student` VALUES (7, '赵日天', 31, '112@qq.com');
 INSERT INTO `student` VALUES (8, '雪花女神龙', 31, '112@qq.com');
 INSERT INTO `student` VALUES (9, '鬼见愁', 49, '123@qq.com');
+INSERT INTO `student` VALUES (10, '雪花女神龙2', 31, '112@qq.com');
+INSERT INTO `student` VALUES (11, '雪花女神龙3', 32, '112@qq.com');
 COMMIT;
 
 -- ----------------------------
